d37dd011ee0a383f919228bc3f1fcc30d983f203
Apoorva Pasbola <pasbola.apoorva@gmail.com>
2021-11-03 20:11:04 +0530

    add basic structure to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const ROUTES:Routes =[
+  {path:'home',component: HomeComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    CoreModule,
+    RouterModule.forRoot(ROUTES)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
